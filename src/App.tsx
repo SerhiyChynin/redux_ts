30113d8315a3e7a51f61437f4f2b9e437e438d3e
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-25 14:27:24 +0300

    getUsers

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
-  const {count} = useAppSelector(state => state.userReducer)
+  // const {count} = useAppSelector(state => state.userReducer)
   const {increment } = userSlice.actions;
   const dispatch = useAppDispatch();
   
   return (
     <div className="App">
-      <h1>{count}</h1>
+      {/* <h1>{count}</h1> */}
       <button onClick={()=> dispatch(increment(10))}>INCREMENT ++</button>
     </div>
   );
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,12 +1,13 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser"
+import { stat } from "fs";
 
 
 interface UserState {
     users: IUser[];
     isLoading: boolean;
     error: string;
-    count: number;
+    // count: number;
 }
 
 
@@ -14,7 +15,7 @@ const initialState:UserState = {
     users: [],
     isLoading: false,
     error: '',
-    count: 0,
+    // count: 0,
 }
 
 export const userSlice = createSlice({
@@ -22,8 +23,24 @@ export const userSlice = createSlice({
     initialState,
     reducers: {
         increment(state, action: PayloadAction<number>) {
-            state.count += action.payload;
-        }
+            // state.count += action.payload;
+        },
+
+        usersFetching(state) {
+            state.isLoading = true;
+        },
+        usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
+            state.isLoading = false;
+            state.error = '';
+            state.users = action.payload;
+            
+        },
+        usersFetchingError(state, action: PayloadAction<string>) {
+            state.isLoading = false;
+            state.error = action.payload;
+            
+        },
+
     }
 })
 
